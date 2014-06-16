e8bfb78d60233e91f02263ef163d88dc86e6e369
breskaggs <breanneskaggs@gmail.com>
2014-06-16 16:00:19 -0600

    Wordpress Site Styling

--- wordpress/wp-content/themes/legit-theme/footer.php
+++ wordpress/wp-content/themes/legit-theme/footer.php
@@ -1,80 +1,82 @@
-    <footer class="container">
-        <div class="row">
-            <nav class="col-sm-2">
-               <h3>Company</h3>
-                <ul>
-                    <li><a href="">About Us</a></li>
-                    <li><a href="">Clients</a></li>
-                    <li><a href="">Partners</a></li>
-                    <li><a href="">Testimonials</a></li>
-                    <li><a href="">Blog</a></li>
-                    <li><a href="">Company</a></li>
-                </ul>
-            </nav>
-            <nav class="col-sm-2">
-               <h3>Overview</h3>
-                <ul>
-                    <li><a href="">What We Do</a></li>
-                    <li><a href="">Why Us</a></li>
-                    <li><a href="">Who We Are</a></li>
-                    <li><a href="">Solutions</a></li>
-                </ul>
-            </nav>
-            <nav class="col-sm-2">
-               <h3>For Clients</h3>
-                <ul>
-                    <li><a href="">Forms</a></li>
-                    <li><a href="">Promotions</a></li>
-                    <li><a href="">Sign In</a></li>
-                    <li><a href="">News</a></li>
-                </ul>
-            </nav>
-            <nav class="col-sm-2">
-               <h3>Company</h3>
-                <ul>
-                    <li><a href="">Home</a></li>
-                    <li><a href="">About Us</a></li>
-                    <li><a href="">Services</a></li>
-                    <li><a href="">Projects</a></li>
-                    <li><a href="">Blog</a></li>
-                    <li><a href="">Contacts</a></li>
-                </ul>
-            </nav>
-            <section class="contact-info col-sm-4">
-                <div class="address">
+    <footer>
+        <div class="container">
+            <div class="row">
+                <nav class="col-sm-2">
+                   <h3>Company</h3>
                     <ul>
-                        <li>
-                        9870 St Vincent Place,
-                        <br>
-                        Glasgow, DC 45 Fr 45.
-                        </li>
-                        <li>
-                            Telephone: +1 800 603 6035
+                        <li><a href="">About Us</a></li>
+                        <li><a href="">Clients</a></li>
+                        <li><a href="">Partners</a></li>
+                        <li><a href="">Testimonials</a></li>
+                        <li><a href="">Blog</a></li>
+                        <li><a href="">Company</a></li>
+                    </ul>
+                </nav>
+                <nav class="col-sm-2">
+                   <h3>Overview</h3>
+                    <ul>
+                        <li><a href="">What We Do</a></li>
+                        <li><a href="">Why Us</a></li>
+                        <li><a href="">Who We Are</a></li>
+                        <li><a href="">Solutions</a></li>
+                    </ul>
+                </nav>
+                <nav class="col-sm-2">
+                   <h3>For Clients</h3>
+                    <ul>
+                        <li><a href="">Forms</a></li>
+                        <li><a href="">Promotions</a></li>
+                        <li><a href="">Sign In</a></li>
+                        <li><a href="">News</a></li>
+                    </ul>
+                </nav>
+                <nav class="col-sm-2">
+                   <h3>Company</h3>
+                    <ul>
+                        <li><a href="">Home</a></li>
+                        <li><a href="">About Us</a></li>
+                        <li><a href="">Services</a></li>
+                        <li><a href="">Projects</a></li>
+                        <li><a href="">Blog</a></li>
+                        <li><a href="">Contacts</a></li>
+                    </ul>
+                </nav>
+                <section class="contact-info col-sm-4">
+                    <div class="address">
+                        <ul>
+                            <li>
+                            9870 St Vincent Place,
                             <br>
-                            FAX: +1 800 889 9898
-                        </li>
-                    </ul>
-                </div>
-                <div class="footer-logo">
-                    <a href="">
-                        <img src="" alt="">
-                    </a>
-                    <p>
-                        <a href="">Crocoblock</a>
-                        &copy; 2014 | 
-                        <a href="">Privacy Policy</a>
-                    </p>
-                </div>
-                <div class="social">
-                    <ul>
-                       <!--Give a-href a background image/sprite-->
-                        <li><a href=""></a></li>
-                        <li><a href=""></a></li>
-                        <li><a href=""></a></li>
-                        <li><a href=""></a></li>
-                    </ul>
-                </div>
-            </section>
+                            Glasgow, DC 45 Fr 45.
+                            </li>
+                            <li>
+                                Telephone: +1 800 603 6035
+                                <br>
+                                FAX: +1 800 889 9898
+                            </li>
+                        </ul>
+                    </div>
+                    <div class="footer-logo">
+                        <a href="">
+                            <img src="" alt="">
+                        </a>
+                        <p>
+                            <a href="">Crocoblock</a>
+                            &copy; 2014 | 
+                            <a href="">Privacy Policy</a>
+                        </p>
+                    </div>
+                    <div class="social">
+                        <ul>
+                           <!--Give a-href a background image/sprite-->
+                            <li><a href=""></a></li>
+                            <li><a href=""></a></li>
+                            <li><a href=""></a></li>
+                            <li><a href=""></a></li>
+                        </ul>
+                    </div>
+                </section>
+            </div>
         </div>
     </footer>
 
--- wordpress/wp-content/themes/legit-theme/index.php
+++ wordpress/wp-content/themes/legit-theme/index.php
@@ -14,10 +14,12 @@
                     
                         <div class="carousel-inner">
                             <div class="item active">
-                                <img src="<?php bloginfo("stylesheet_directory"); ?>/img/slider-1.jpg" alt="">
+                                <img src="<?php bloginfo("stylesheet_directory"); ?>/img/slider-5.jpg" alt="">
                                 <div class="container">
                                         <div class="carousel-caption">
-                                            <h2>Add a Sense <span>of Comfort to Your Living Space</span></h2>
+                                            <h2>Add a Sense 
+                                            <br>
+                                            <span>of Comfort to Your Living Space</span></h2>
                                             <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Praesentium blanditiis dicta porro quibusdam delectus adipisci!</p>
                                             <p><a href="" class="btn btn-lg btn-primary">Sign Up Today</a></p>
                                         </div>
@@ -25,10 +27,12 @@
                             </div> 
                     
                             <div class="item">
-                                <img src="<?php bloginfo("stylesheet_directory"); ?>/img/slider-2.jpg" alt="">
+                                <img src="<?php bloginfo("stylesheet_directory"); ?>/img/slider-6.jpg" alt="">
                                 <div class="container">
                                         <div class="carousel-caption">
-                                            <h2>Create Ideas <span>for Your Interior Design</span></h2>
+                                            <h2>Create Ideas 
+                                            <br>
+                                            <span>for Your Interior Design</span></h2>
                                             <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Perspiciatis, iure facere harum. Veritatis obcaecati quae magnam natus alias vitae dolores.</p>
                                             <p><a href="" class="btn btn-lg btn-primary">Learn More</a></p>
                                         </div>
@@ -36,7 +40,7 @@
                                 </div> 
                     
                             <div class="item">
-                                <img src="<?php bloginfo("stylesheet_directory"); ?>/img/slider-3.jpg" alt="">
+                                <img src="<?php bloginfo("stylesheet_directory"); ?>/img/slider-7.jpg" alt="">
                                 <div class="container">
                                     <div class="carousel-caption">
                                         <h2>One more for good measure.</h2>
@@ -76,7 +80,7 @@
         
         <!---Tips--->
         
-        <section class="red-tips-row">
+        <section class="red-tips">
             <div class="container">
                 <div class="tips row">
                     <article class="col-sm-4 clearfix">
@@ -91,7 +95,7 @@
                         <img class="pull-left" src="<?php bloginfo("stylesheet_directory"); ?>/img/leaf.png" alt="">
                         <div class="tips-caption">
                             <h2>Eco Materials</h2>
-                            <p>Lorem ipsum dolor sit amet, consectetur.</p>
+                            <p>Lorem ipsum dolor sit amet.</p>
                             <a href="">Read More</a>
                         </div>
                     </article>
@@ -109,77 +113,81 @@
         
         <!---Latest--->
         
-        <section class="latest container">
-            <div class="row">
-                <h2>Our Latest Projects</h2>
-                <article>
-                    <img src="" alt="">
-                    <h3>Massa as laoreet</h3>
-                    <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Numquam ipsum, rem assumenda, reiciendis quas illum!</p>
-                    <button>More</button>
-                </article>
-                <article>
-                    <img src="" alt="">
-                    <h3>Lorem ipsum est</h3>
-                    <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Modi possimus quasi nesciunt esse dolor ducimus.</p>
-                    <button>More</button>
-                </article>
-                <article>
-                    <img src="" alt="">
-                    <h3>Dolore massa</h3>
-                    <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Sed similique iste, dolores, optio totam ipsam!</p>
-                    <button>More</button>
-                </article>
-                   <article>
-                    <img src="" alt="">
-                    <h3>Magna ut dolore</h3>
-                    <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Voluptatum soluta consectetur in numquam rerum nihil!</p>
-                    <button>More</button>
-                </article>
-            </div>
-        </section>
+        <div class="content-bg">
+            <section class="latest container">
+                <div class="row">
+                    <h2>Our Latest Projects</h2>
+                    <article class="col-sm-3">
+                        <img class="img-responsive" src="<?php bloginfo("stylesheet_directory"); ?>/img/article-1-crop.jpg" alt="">
+                        <h3>Massa as laoreet</h3>
+                        <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Numquam ipsum, rem assumenda, reiciendis quas illum!</p>
+                        <button>More</button>
+                    </article>
+                    <article class="col-sm-3">
+                        <img class="img-responsive" src="<?php bloginfo("stylesheet_directory"); ?>/img/article-2-crop.jpg" alt="">
+                        <h3>Lorem ipsum est</h3>
+                        <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Modi possimus quasi nesciunt esse dolor ducimus.</p>
+                        <button>More</button>
+                    </article>
+                    <article class="col-sm-3">
+                        <img class="img-responsive" src="<?php bloginfo("stylesheet_directory"); ?>/img/article-3-crop.jpg" alt="">
+                        <h3>Dolore massa</h3>
+                        <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Sed similique iste, dolores, optio totam ipsam!</p>
+                        <button>More</button>
+                    </article >
+                       <article class="col-sm-3">
+                        <img class="img-responsive" src="<?php bloginfo("stylesheet_directory"); ?>/img/article-4-crop.jpg" alt="">
+                        <h3>Magna ut dolore</h3>
+                        <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Voluptatum soluta consectetur in numquam rerum nihil!</p>
+                        <button>More</button>
+                    </article>
+                </div>
+            </section>
         
-        <!---Welcome--->
-        
-        <section class="welcome container">
-            <div class="row">
+            <!---Welcome--->
+
+            <section class="welcome container">
                 <h2>Welcome to Our Site!</h2>
                 <h3>We offer the largest assortment of unique items for your home! Style, comfort, quality, selection!</h3>
                 <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. At illum cum consectetur debitis voluptatem incidunt necessitatibus fugiat, officiis praesentium perferendis beatae veritatis, rem minima ut porro odio, perspiciatis neque quam esse eaque sequi enim dicta? Vero magnam error ipsa deserunt hic in eius? Placeat ratione sit deserunt repudiandae, est quibusdam.</p>
                 <button>Read More</button>
-            </div>
-        </section>
-        <section class="about-video container">
-            <div class="row">
-                <article class="about col-xs-12 col-sm-6">
-                    <h2>About Our Team</h2>
-                    <div>
-                        <img src="" alt="">
-                        <h4>Lorem Ipsum Dolore
-                            <span>Massa Laoreetum</span>
-                        </h4>
-                            <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Vel tempore esse error repudiandae ipsum libero, deleniti debitis dolorem facere est.</p>
-                            <button>More</button>
-                    </div>
-                </article>
-                <article class="video col-xs=-12 col-sm-6">
-                    <h2>Latest Video</h2>
-                    <a href="">
-                        <img src="" alt="">
-                        <span></span>
-                    </a>
-                </article>
-            </div>
-        </section>
-        <section class="clients container">
-            <div class="row">
-                <h2 class="col-sm-2">Our Most Recent Clients</h2>
-                <img src="" alt="" class="col-sm-2">
-                <img src="" alt="" class="col-sm-2">
-                <img src="" alt="" class="col-sm-2">
-                <img src="" alt="" class="col-sm-2">
-                <img src="" alt="" class="col-sm-2">
-            </div>
-        </section>
+            </section>
+
+           <!---About/Video--->
+           
+            <section class="about-video container">
+                <div class="row">
+                    <article class="about col-xs-12 col-sm-6">
+                        <h2>About Our Team</h2>
+                        <div>
+                            <img class="img-responsive" src="<?php bloginfo("stylesheet_directory"); ?>/img/about.png" alt="">
+                            <h4>Lorem Ipsum Dolore
+                                <span>Massa Laoreetum</span>
+                            </h4>
+                                <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Vel tempore esse error repudiandae ipsum libero, deleniti debitis dolorem facere est.</p>
+                                <button>More</button>
+                        </div>
+                    </article>
+                    <article class="video col-xs=-12 col-sm-6">
+                        <h2>Latest Video</h2>
+                        <a href="">
+                            <img class="img-responsive" src="<?php bloginfo("stylesheet_directory"); ?>/img/video.jpg" alt="">
+                            <span></span>
+                        </a>
+                    </article>
+                </div>
+            </section>
+
+            <section class="clients container">
+                <div class="row">
+                    <h2 class="col-sm-2">Our Most Recent Clients</h2>
+                    <img src="<?php bloginfo("stylesheet_directory"); ?>/img/client-1.png" alt="" class="col-sm-2 img-responsive">
+                    <img src="<?php bloginfo("stylesheet_directory"); ?>/img/client-2.png" alt="" class="col-sm-2 img-responsive">
+                    <img src="<?php bloginfo("stylesheet_directory"); ?>/img/client-3.png" alt="" class="col-sm-2 img-responsive">
+                    <img src="<?php bloginfo("stylesheet_directory"); ?>/img/client-4.png" alt="" class="col-sm-2 img-responsive">
+                    <img src="<?php bloginfo("stylesheet_directory"); ?>/img/client-5.png" alt="" class="col-sm-2 img-responsive">
+                </div>
+            </section>
+        </div>
     </main>
 <?php get_footer() ?>
